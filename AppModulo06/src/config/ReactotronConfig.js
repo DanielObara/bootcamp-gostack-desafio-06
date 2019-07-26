b1d4933c78c95d8b13c35a147a7db5567e72016f
daniel.obara <daniel.tsutomu@hotmail.com>
2019-07-26 14:23:14 -0300

    Reactotron, Eslint, Prettier, Editorconfig also configured

---
+++ AppModulo06/src/config/ReactotronConfig.js
@@ -0,0 +1,14 @@
+import Reactotron from 'reactotron-react-native';
+
+if (__DEV__) {
+	const tron = Reactotron.configure({ host: '127.0.0.1' })
+		.useReactNative()
+		// .useReactNative(asyncStorage())
+		// .use(reactotronRedux())
+		// .use(reactotronSaga())
+		.connect();
+
+	tron.clear();
+
+	console.tron = tron;
+}
